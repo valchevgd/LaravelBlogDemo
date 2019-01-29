5cb2796fa916f881377c3b5480cff226a29717c3
Georgi Valchev <valchevgd@gmail.com>
2019-01-29 22:47:54 +0200

    Article model

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -1,69 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Our Stories</title>
-    <link rel="stylesheet" href="{{'../../css/style.css'}}">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
-          integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-</head>
-<body>
-<header>
-    <nav>
+@extends('base')
+
+@section('title', ' | About')
+
+@section('main')
+    <main>
+
         <article>
-            <p>Our <i class="fas fa-pen-nib"></i> Stories</p>
+            <h3>WHO WE ARE</h3>
+            <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and
+                demoralized by the charms of pleasure.</p>
         </article>
 
-        <ul>
-            <li><a href="{{'/'}}">HOME</a></li>
-            <li><a href="{{'about'}}">ABOUT</a></li>
-        </ul>
-    </nav>
-</header>
-<main>
+        <article>
+            <h3>WHY WE ARE HERE</h3>
+            <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and
+                demoralized by the charms of pleasure.</p>
+        </article>
 
-    <article>
-        <h3>WHO WE ARE</h3>
-        <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and
-            demoralized by the charms of pleasure.</p>
-    </article>
-
-    <article>
-        <h3>WHY WE ARE HERE</h3>
-        <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and
-            demoralized by the charms of pleasure.</p>
-    </article>
-
-    <article>
-        <h3>CONTACT US</h3>
-        <form action="post">
-            <p>
-                <label for="name">Name :</label>
-                <input type="text" id="name" placeholder="Please enter your name...">
-            </p>
-            <p>
-                <label for="Email">Email :</label>
-                <input type="email" id="email" placeholder="Please enter your email...">
-            </p>
-            <p>
-                <label for="message">Message :</label>
-                <textarea id="message" cols="30" rows="10" placeholder="Please enter your message to us..."></textarea>
-            </p>
-        </form>
-    </article>
-
-</main>
-<footer>
-    <article>
-        &copy; Copyright valchevgd.
-    </article>
-
-    <article>
-        <a href="#"><i class="fab fa-facebook-f"></i></a>
-        <a href="#"><i class="far fa-envelope"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
-        <a href="#"><i class="fab fa-pinterest-p"></i></a>
-    </article>
-</footer>
-</body>
-</html>
+        <article class="contact-form">
+            <h3>CONTACT US</h3>
+            <form action="post">
+                <p>
+                    <label for="name">Name :</label>
+                    <input type="text" id="name" placeholder="Please enter your name...">
+                </p>
+                <p>
+                    <label for="Email">Email :</label>
+                    <input type="email" id="email" placeholder="Please enter your email...">
+                </p>
+                <p>
+                    <label for="message">Message :</label><br>
+                    <textarea id="message" cols="30" rows="10"
+                              placeholder="Please enter your message to us..."></textarea>
+                </p>
+            </form>
+        </article>
+    </main>
+@endSection
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Our Stories</title>
-    <link rel="stylesheet" href="{{'../../css/style.css'}}">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
-          integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-</head>
-<body>
-<header>
-    <nav>
-        <article>
-            <p>Our <i class="fas fa-pen-nib"></i> Stories</p>
-        </article>
+@extends('base')
 
-        <ul>
-            <li><a href="{{'/'}}">HOME</a></li>
-            <li><a href="{{'about'}}">ABOUT</a></li>
-        </ul>
-    </nav>
-</header>
+@section('title', ' | Homepage')
+
+@section('main')
 <main>
     <section>
         <h1><span>Welcome</span><br> <span>to</span><br> Our <i class="fas fa-pen-nib"></i> Stories</h1>
@@ -33,17 +16,4 @@
         </article>
     @endforeach
 </main>
-<footer>
-    <article>
-        &copy; Copyright valchevgd.
-    </article>
-
-    <article>
-        <a href="#"><i class="fab fa-facebook-f"></i></a>
-        <a href="#"><i class="far fa-envelope"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
-        <a href="#"><i class="fab fa-pinterest-p"></i></a>
-    </article>
-</footer>
-</body>
-</html>
+@endsection
