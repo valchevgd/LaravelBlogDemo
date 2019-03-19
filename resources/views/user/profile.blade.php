1f1fe1d8bb79d6a662a77481a83639d9ce30a7cb
Georgi Valchev <valchevgd@gmail.com>
2019-03-19 17:13:46 +0200

    Rendering User's Picture in Profile (end of project at this point)

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -6,12 +6,16 @@
     <section class="profile">
         <section id="profile">
             <h3>Hello, {{Auth::user()->name}}!</h3>
-            <form action="{{route('upload_picture')}}" method="post" enctype="multipart/form-data">
-                <input type="file" name="pic" accept="image/*">
-                <input type="submit">
-                @csrf
-            </form>
+            <div class="image">
+                <img src="{{asset('img/' . Auth::user()->image)}}" alt="profile_picture">
+                <form action="{{route('upload_picture')}}" method="post" enctype="multipart/form-data">
+                    <input type="file" name="pic" accept="image/*">
+                    <input class="button" type="submit">
+                    @csrf
+                </form>
+            </div>
         </section>
+
         <section class="my-stories">
             <h3>My Stories</h3>
             @foreach($stories as $story)
@@ -23,7 +27,8 @@
                     <div>
                         <a href="{{route('show_story', ['id' => $story->id])}}"><i class="fas fa-bars"> View</i></a>
                         <a href="{{route('edit_story', ['id' => $story->id])}}"><i class="fas fa-edit"> Edit</i></a>
-                        <a href="{{route('delete_story', ['id' => $story->id])}}"><i class="fas fa-trash-alt"> Delete</i></a>
+                        <a href="{{route('delete_story', ['id' => $story->id])}}"><i class="fas fa-trash-alt">
+                                Delete</i></a>
                     </div>
                 </article>
             @endforeach
